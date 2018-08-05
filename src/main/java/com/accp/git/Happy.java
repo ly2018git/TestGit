a96548f6618c7e006459aa283e1077c2dfc99175
eclipse_user <eclipse_user@foxmail.com>
2018-08-05 18:04:19 +0800

    commit happy

---
+++ src/main/java/com/accp/git/Happy.java
@@ -0,0 +1,16 @@
+package com.accp.git;
+
+/**
+ * 项目名称：TestGit
+ * 类名称：Happy
+ * 类描述：
+ * 创建时间：2018年8月5日下午6:03:29
+ * @author YuanLi
+ * 修改人：YuanLi
+ * 修改时间：2018年8月5日下午6:03:29
+ * 修改备注：
+ * @version
+ */
+public interface Happy {
+
+}
