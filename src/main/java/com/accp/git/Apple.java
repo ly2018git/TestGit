e8c6a82f264a489bea1c84122033ee540d60108a
eclipse_user <eclipse_user@foxmail.com>
2018-08-05 18:03:13 +0800

    My first commit in eclipse

---
+++ src/main/java/com/accp/git/Apple.java
@@ -0,0 +1,16 @@
+package com.accp.git;
+
+/**
+ * 项目名称：TestGit
+ * 类名称：Apple
+ * 类描述：
+ * 创建时间：2018年8月5日下午3:56:26
+ * @author YuanLi
+ * 修改人：YuanLi
+ * 修改时间：2018年8月5日下午3:56:26
+ * 修改备注：
+ * @version
+ */
+public class Apple {
+
+}
---
+++ src/test/java/com/accp/test/GoodTest.java
@@ -0,0 +1,16 @@
+package com.accp.test;
+
+/**
+ * 项目名称：TestGit
+ * 类名称：GoodTest
+ * 类描述：
+ * 创建时间：2018年8月5日下午3:57:24
+ * @author YuanLi
+ * 修改人：YuanLi
+ * 修改时间：2018年8月5日下午3:57:24
+ * 修改备注：
+ * @version
+ */
+public class GoodTest {
+
+}
